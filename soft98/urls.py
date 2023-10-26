bb7c763423950abee1dba5cd599f750420b3973c
Hussein <answereeee4@gmail.com>
2023-10-26 03:49:42 +0330

    update urls file

--- soft98/urls.py
+++ soft98/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('news.urls'), name='home'),
-    path ('accounts/', include('accounts.urls'), name='accounts'),
+    path('', include('news.urls'), name='home'),
+    path('accounts/', include('accounts.urls'), name='accounts'),
+    path('api/', include('api.urls'), name='api'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
