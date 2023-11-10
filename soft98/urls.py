d97dd243fe4f4064a2e8deeeaa0430813f70b1b9
Ho139523 <99872823+Ho139523@users.noreply.github.com>
2023-11-11 01:21:04 +0330

    Update urls.py

--- soft98/urls.py
+++ soft98/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from azbankgateways.urls import az_bank_gateways_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls'), name='home'),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('api/', include('api.urls'), name='api'),
+    path('bankgateways/', az_bank_gateways_urls()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
